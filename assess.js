8d2e8ab51a34e52fb373a3cbcacfe6631c057a7c
RishuPatel <insyt.org@gmail.com>
2024-06-02 22:22:27 +0530

    first commit

---
+++ assess.js
@@ -0,0 +1,58 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+
+const NFTs = [];
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_name, _size, _weapon, _hitPoints, _aura) {
+   const NFT={
+    "name" : _name,
+    "size" : _size,
+    "weapon" : _weapon,
+    "hitpoints" : _hitPoints,
+    "aura" : _aura
+   }
+
+   NFTs.push(NFT);
+   console.log("minted:  " + _name);
+   
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+   for(let i = 0; i< NFTs.length; i++){
+    console.log("\nID : " + (i+1));
+    console.log("Name : " + NFTs[i].name);
+    console.log("Size : " + NFTs[i].size);
+    console.log("Weapon : " + NFTs[i].weapon);
+    console.log("HitPoints : " + NFTs[i].hitPoints);
+    console.log("Aura : " + NFTs[i].aura);
+   }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log("\nNumber of NFTs minted : " + NFTs.length);
+}
+
+// call your functions below this line
+
+
+
+mintNFT("Ubel", "Cute", "PoleArm", "Infinite", "Mommy");
+mintNFT("Frieren", "OldHag", "NerdNature", "FinalBoss", "LostKid");
+mintNFT("Fern", "Thicc", "DeathStare", "Unsure", "StrictMum");
+listNFTs();
+getTotalSupply();
